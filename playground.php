9292507d1ea8a4798a8d83ff7efd5f3c3cf88875
Andrew Aday <andrewladay@gmail.com>
2015-08-21 19:27:45 -0400

    stuff

--- playground.php
+++ playground.php
@@ -1,11 +1,17 @@
+
 <?php
 
 $norms_path = "AP_ITEM_DATASET/5.14/NORMAL_5X5/key_1/";
+require_once 'includes/db_connect.php';
 
 foreach(scandir($norms_path) as $file) {
 		if ('.' === $file) continue;
         if ('..' === $file) continue;
 		print_r(strtolower(basename($file, ".json").PHP_EOL));
 	}
+
+$doc = $matches->findOne();
+print_r($doc);
+
 
 ?>
