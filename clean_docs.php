f1b1ab5c38a3df212c269202ef03f70c76ec22bf
Andrew Aday <andrewladay@gmail.com>
2015-08-21 20:50:19 -0700

    faew

--- clean_docs.php
+++ clean_docs.php
@@ -6,14 +6,23 @@ include 'includes/major_items.php';
 $number = $matches->count();
 $counter = 1;
 $cursor = $matches->find();
-foreach($cursor as $doc) {
-	$matchId = $doc['matchId'];
+// foreach($cursor as $doc) {
+// 	$matchId = $doc['matchId'];
+// 	if ($matches->remove(['matchId' => $matchId])) {
+// 		print_r($matchId . " removed " . $counter . "/" . $number . "\n");
+// 	}
+// 	$match_trim = parseMatch($doc,$MAJOR_ITEMS);
+// 	$matches->insert($match_trim);
+// 	$counter++;
+// }
+
+$doc = $matches->findOne();
+$matchId = $doc['matchId'];
 	if ($matches->remove(['matchId' => $matchId])) {
 		print_r($matchId . " removed " . $counter . "/" . $number . "\n");
 	}
 	$match_trim = parseMatch($doc,$MAJOR_ITEMS);
 	$matches->insert($match_trim);
 	$counter++;
-}
 
 ?>
