8db4d995501d54ce3c2fdef5050c2753c9102a1e
Winston Wright <winstonwright88@gmail.com>
2015-08-28 01:24:30 -0700

    Tiny index changes
    Keep those commits going

--- index.php
+++ index.php
@@ -3,4 +3,16 @@ $champions = json_decode(file_get_contents('includes/static-data/champions.json'
 $champNames=[];
 foreach($champions['data'] as $name=>$champion);
 	$champNames[$champion['key']]=$name;
+if(!isset($_GET['champion']))
+{
+	foreach($champNames as $value)
+	{
+		echo "<a href='./?champion=$value'>$value</a><br>";
+	}
+}
+else
+{
+	//show the builds
+	//calculate econ
+}
 ?>
