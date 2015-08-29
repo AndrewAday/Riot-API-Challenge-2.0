5c1d474718e3e09206aa04f76aa6db58aa2383aa
Winston Wright <winstonwright88@gmail.com>
2015-08-29 00:52:02 -0700

    more small changes to index
    got rid of some really dumb garbage

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 $champions = json_decode(file_get_contents('includes/static-data/champions.json'), true);
-$champNames=[];
-foreach($champions['data'] as $name=>$champion);
-	$champNames[$champion['key']]=$name;
 if(!isset($_GET['champion']))
 {
 	foreach($champNames as $value)
@@ -12,6 +9,8 @@ if(!isset($_GET['champion']))
 }
 else
 {
+	$championsEcon = json_decode(file_get_contents('includes/static-data/champions.json'), true);
+	$champion=$championsEcon[$champions['data'][$_GET['champion']]['key'];
 	//show the builds
 	//calculate econ
 }
